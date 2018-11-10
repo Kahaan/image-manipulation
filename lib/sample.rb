e62e6206c9b39fd6c48c5399f77cec0529ddbe3b
Kahaan <kahaan.r.patel@gmail.com>
2018-11-09 18:25:34 -0800

    encoding image

--- lib/sample.rb
+++ lib/sample.rb
@@ -10,13 +10,25 @@ class ImageManipulator
     @image_path = image_path
     @image = ChunkyPNG::Image.from_file(@image_path)
     @copy = @image
-    @bits = img_to_binary
+    @bits = img_to_binary(@copy)
+    # @abc = binary_to_image(@bits)
   end
 
-  def img_to_binary
-    color_values = @copy.pixels
+  def img_to_binary(image)
+    color_values = image.pixels
     rgba_values = color_values.map { |colorval| ChunkyPNG::Color.to_truecolor_alpha_bytes(colorval) }
     bits = rgba_values.map { |pixel| pixel.map{|rgba| rgba.to_s(2)} }
+  end
+
+  def binary_to_image(binary)
+# I'm modifying the bits of the image I read in but not reassembling the bits into a new encrypted image.
+# The goal is to add the number of bits to extract to the last r value of the pixel and extract the same number of bits and assemble the message
+    rgba = binary.map { |pixel| pixel.map{|rgba| rgba.to_i(2)} }
+    canvas = ChunkyPNG::Canvas.from_rgba_stream(200,200,rgba)
+    # puts canvas
+    encoded_image = canvas.to_image
+    # puts encoded_image.class
+    encoded_image.save('../encoded_pic.png')
   end
 
   def text_to_binary(text)
@@ -36,6 +48,7 @@ class ImageManipulator
 
     i = 0
     j = 0
+    count = 0
     # puts "#{message_in_bits}:"
     while  i <= message_in_bits.length
 
@@ -53,13 +66,22 @@ class ImageManipulator
 
       i += 3
       j += 1
+      count += 3
     end
-    @copy.save(File.open("../encoded_pic.png", 'w+'))
+# What if the count of bits edited is greater than 255?
+    # @bits[-1][0] = count.to_s(2)
+    # print binary_to_image(@bits)
+
+
+    # print img_to_binary(@copy)[-1]
+    # @bits.save(File.open("../encoded_pic.png", 'w+'))
   end
 
-  def decode_message(text)
-
-    length_of_text = text_to_binary(text).length
+  def decode_message(encoded_image_path="../encoded_pic.png")
+    encoded_image = ChunkyPNG::Image.from_file(encoded_image_path)
+    encoded_bits = img_to_binary(encoded_image)
+    puts length_of_text = encoded_bits[-1]
+    # length_of_text = text_to_binary(text).length
     results = []
 
       i = 0
@@ -74,6 +96,7 @@ class ImageManipulator
       end
 
       message_in_binary = [results[0...length_of_text].join("")]
+      # puts message_in_binary
       puts binary_to_text(message_in_binary)
   end
 
@@ -81,8 +104,9 @@ end
 
 
 img = ImageManipulator.new('../PNG-image.png')
-img.encode("send nudes")
-img.decode_message("send nudes")
+# puts binary_to_image(img.bits)
+# img.encode("send nudes")
+# img.decode_message("../encoded_pic.png")
 
 
 
