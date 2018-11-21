dde5bf7bc3de30b1844da6bf9e838248cd75fe0f
Kahaan <kahaan.r.patel@gmail.com>
2018-11-21 10:38:17 -0800

    encoding

--- tool/script.rb
+++ tool/script.rb
@@ -3,6 +3,6 @@ require_relative '../lib/image.rb'
 
 img = ImageManipulator.new('../IronMonkey.png')
 #
-img.encode("Old macdonald had a farm, E-I-E-I-O", '../abcd.png')
+img.encode("Old macdonald had a farm, E-I-E-I-O", '../xyz.png')
 #
-img.decode_message('../abcd.png')
+img.decode_message('../xyz.png')
