b92ef5bcea5d9be96f4fb6322ac9c0ec689b6791
Kahaan <kahaan.r.patel@gmail.com>
2019-03-24 20:37:12 -0700

    minor refactoring

--- tool/script.rb
+++ tool/script.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/image.rb'
 
 
-img = ImageManipulator.new('../IronMonkey.png')
+# img = ImageManipulator.new('image-hide-n-seek.s3.amazonaws.com/posts/images/000/000/075/original/img8.png')
+img = ImageManipulator.new('../png1.png')
 
-img.encode('Random message')
-
-img.decode_message
+img.encode("no message yet")
+# img.decode_message
